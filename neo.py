05e4d27bf65f41de229dfee7fc97f8abcc412669
kavin <kavins583224148024@nprcolleges.org>
2025-10-17 12:00:20 +0530

    successfully completed

---
+++ neo.py
@@ -0,0 +1,26 @@
+a = 10
+b = 5
+
+
+sum_result = a + b
+print("Addition:", sum_result)
+
+
+sub_result = a - b
+print("Subtraction:", sub_result)
+
+
+mul_result = a * b
+print("Multiplication:", mul_result)
+
+
+div_result = a / b
+print("Division:", div_result)
+
+
+mod_result = a % b
+print("Modulus:", mod_result)
+
+
+exp_result = a ** b
+print("Exponentiation:", exp_result)
